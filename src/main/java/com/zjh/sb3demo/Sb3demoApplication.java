e2c3cb5a34d0e6e7fe88d32807640515b61658c2
zhaomsdemo <zhaomsdemo@gmail.com>
2022-12-03 14:50:01 -0800

    Enable AspectJ

--- src/main/java/com/zjh/sb3demo/Sb3demoApplication.java
+++ src/main/java/com/zjh/sb3demo/Sb3demoApplication.java
@@ -3,9 +3,11 @@ package com.zjh.sb3demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
 @EnableConfigurationProperties
+@EnableAspectJAutoProxy
 public class Sb3demoApplication {
 
     public static void main(String[] args) {
