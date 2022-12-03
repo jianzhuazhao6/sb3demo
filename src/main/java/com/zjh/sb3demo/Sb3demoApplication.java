00a373d3693e0158c0417b79b3e0e66108ef3b1d
zhaomsdemo <zhaomsdemo@gmail.com>
2022-12-03 13:37:58 -0800

    init

---
+++ src/main/java/com/zjh/sb3demo/Sb3demoApplication.java
@@ -0,0 +1,13 @@
+package com.zjh.sb3demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sb3demoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sb3demoApplication.class, args);
+    }
+
+}
