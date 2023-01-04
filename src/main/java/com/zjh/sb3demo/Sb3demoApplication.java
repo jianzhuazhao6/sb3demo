fabda2d7802e731baa6cd16bec812274be6b038b
zhaomsdemo <zhaomsdemo@gmail.com>
2023-01-04 12:32:39 -0800

    check in

--- src/main/java/com/zjh/sb3demo/Sb3demoApplication.java
+++ src/main/java/com/zjh/sb3demo/Sb3demoApplication.java
@@ -4,10 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @SpringBootApplication
 @EnableConfigurationProperties
 @EnableAspectJAutoProxy
+@EnableWebSecurity
 public class Sb3demoApplication {
 
     public static void main(String[] args) {
